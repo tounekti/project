2ef48e128c485074f463da9019aebcaeee43276e
azizbkk <mouhamedazizbenkhaled@gmail.com>
2020-06-02 11:31:14 +0200

    last version

--- idrecords.py
+++ idrecords.py
@@ -11,6 +11,28 @@ urls = ("/", "Index",
 		"/privacypolicy", "PrivacyPolicy",
 		"/contact", "Contact",
         "/account", "Account",
+        "/cart", "Cart",
+        "/vinyl", "Vinyl",
+        "/cd_dvd", "CdDvd",
+        "/sale", "Sale",
+        "/vinyl_new", "VinylNew",
+        "/tapes_new_hiphop", "TapesNewHiphop",
+        "/merch_clothing_rekords", "MerchClothingRekordsProduct",
+        "/merch_clothing_dengale", "MerchClothingDengale",
+        "/cd_dvd_usuk", "CdDvdUsUk",
+        "/cd_dvd_scand_singles", "CdDvdScandSinglesProduct",
+        "/cd_dvd_scand_albums", "CdDvdScandAlbums",
+        "/customer_sitemap", "CustomerSiteMap",
+        "/extras_voucher", "ExtrasVoucher",
+        "/used_tapes_hiphop", "UsedTapesHiphop",
+        "/newvinyl_dance_hiphop", "NewVinylDanceHiphop",
+        "/newninyl_funk_soul", "NewVinylFunkSoul",
+        "/newvinyl_hiphop", "NewVinylHiphop",
+        "/newvinyl_jazz", "NewVinylJazz",
+        "/newvinyl_reggae", "NewVinylReggae",
+        "/newvinyl_world", "NewVinylWorld",
+        "/newvinyl_beats", "NewVinylBeats",
+        "/new_tapes_hiphop", "NewTapesHiphop",
 		)
 
 
@@ -57,9 +79,141 @@ class Account:
         """ Show page """
         return render.account(urlapp)        
 
+class Cart:
+
+    def GET(self):
+        """ Show page """
+        return render.cart(urlapp)
+        
+class Vinyl:
+
+    def GET(self):
+        """ Show page """
+        return render.vinyl(urlapp)
+
+class CdDvd:
+
+    def GET(self):
+        """ Show page """
+        return render.cd_dvd(urlapp)           
+
+class Sale:
+
+    def GET(self):
+        """ Show page """
+        return render.sale(urlapp)
+        
+class VinylNew:
+
+    def GET(self):
+        """ Show page """
+        return render.vinyl_new(urlapp) 
+
+class TapesNewHiphop:
+
+    def GET(self):
+        """ Show page """
+        return render.tapes_new_hiphop(urlapp) 
+        
+
+class CdDvdScandSinglesProduct:
+
+    def GET(self):
+        """ Show page """
+        return render.cd_dvd_scand_singles(urlapp) 
+        
+class CdDvdScandAlbums:
+
+    def GET(self):
+        """ Show page """
+        return render.cd_dvd_scand_albums(urlapp)     
+        
+class  CdDvdUsUk:
+
+    def GET(self):
+        """ Show page """
+        return render.cd_dvd_usuk(urlapp)          
+        
+        
+class MerchClothingRekordsProduct:
+
+    def GET(self):
+        """ Show page """
+        return render.merch_clothing_rekords(urlapp)         
+
+class MerchClothingDengale:
+
+    def GET(self):
+        """ Show page """
+        return render.merch_clothing_dengale(urlapp)    
 
 
+class CustomerSiteMap:
 
+    def GET(self):
+        """ Show page """
+        return render.customer_sitemap(urlapp)          
+
+class ExtrasVoucher:
+
+    def GET(self):
+        """ Show page """
+        return render.extras_voucher(urlapp)   
+        
+class UsedTapesHiphop:
+
+    def GET(self):
+        """ Show page """
+        return render.used_tapes_hiphop(urlapp)  
+        
+class NewVinylDanceHiphop:
+
+    def GET(self):
+        """ Show page """
+        return render.newvinyl_dance_hiphop(urlapp)       
+
+class NewVinylFunkSoul:
+
+    def GET(self):
+        """ Show page """
+        return render.newninyl_funk_soul(urlapp)      
+
+class NewVinylHiphop:
+
+    def GET(self):
+        """ Show page """
+        return render.newvinyl_hiphop(urlapp) 
+
+class NewVinylJazz:
+
+    def GET(self):
+        """ Show page """
+        return render.newvinyl_jazz(urlapp)    
+
+
+class NewVinylReggae:
+
+    def GET(self):
+        """ Show page """
+        return render.newvinyl_reggae(urlapp)         
+        
+class NewVinylWorld:
+
+    def GET(self):
+        """ Show page """
+        return render.newvinyl_world(urlapp)       
+
+class NewVinylBeats:
+
+    def GET(self):
+        """ Show page """
+        return render.newvinyl_beats(urlapp)    
+
+class NewTapesHiphop:
+
+    def GET(self):
+        """ Show page """
+        return render.new_tapes_hiphop(urlapp)          
 
 app = web.application(urls, globals())
 
